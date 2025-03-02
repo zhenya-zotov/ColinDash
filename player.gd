0b60a4c209e7f4014b351261b780bbe0c58d86d4
Zhenya Zotov <zhenya.zotov.1996@gmail.com>
2025-03-02 18:52:41 +0100

    Fix bugs

--- player.gd
+++ player.gd
@@ -35,7 +35,7 @@ func _process(delta: float) -> void:
 func start():
 	set_process(true)
 	position = screensize / 2
-	$AnimatedSprite2D.animation = "indle"
+	$AnimatedSprite2D.animation = "idle"
 	
 func die():
 	$AnimatedSprite2D.animation = "hurt"
