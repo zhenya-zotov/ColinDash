0c3d8b0a84fa890a4583cb2c986956d5517dffa9
Zhenya Zotov <zhenya.zotov.1996@gmail.com>
2025-03-02 04:55:18 +0100

    First commit

---
+++ player.gd
@@ -0,0 +1,46 @@
+extends Area2D
+
+signal pickup
+signal hurt
+
+@export var speed = 350
+var velocity = Vector2.ZERO
+var screensize = Vector2(480, 720)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	position += velocity * speed * delta
+	
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	
+	if velocity.length() > 0:
+		$AnimatedSprite2D.animation = "run"
+	else:
+		$AnimatedSprite2D.animation = "idle"
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+		
+func start():
+	set_process(true)
+	position = screensize / 2
+	$AnimatedSprite2D.animation = "indle"
+	
+func die():
+	$AnimatedSprite2D.animation = "hurt"
+	set_process(false)
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("coins"):
+		area.pickup()
+		pickup.emit()
+	
+	if area.is_in_group("obstacles"):
+		hurt.emit()
+		die()
